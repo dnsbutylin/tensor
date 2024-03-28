31871d381d7e668afbc32a71746e35dc7d9356fb
Denis <dnsbutylin@gmail.com>
2024-03-28 10:36:29 +0300

    feat Реализовано тестовое задание

--- tests/test_1.py
+++ tests/test_1.py
@@ -8,32 +8,50 @@ from src.pages.tensor_page import Tensor
 
 
 def test_1(browser: Remote) -> None:
+    # Создание объекта страницы Sbis и открытие в браузере страницы sbis.ru
     sbis_page = Sbis(browser)
     sbis_page.get("https://sbis.ru")
+
+    # Находим и кликаем на кнопку контактов на странице Sbis
     sbis_page.contacts_button.click()
 
+    # Создание объекта страницы Contacts для работы с ней
     contacts_page = Contacts(browser)
+
+    # Клик по кнопке, связанной с компанией Tensor на странице контактов
     contacts_page.tensor_button.click()
+
+    # Переключаемся на последнюю открытую вкладку браузера
     contacts_page.switch_to_last_tab()
 
+    # Следующий шаг - закрытие уведомления о куки на новой странице
     cookie_page = Cookie(browser)
     cookie_page.tensor_cookie_close_button.click()
 
+    # Создание объекта страницы Tensor и выполнение действий на ней
     tensor_page = Tensor(browser)
+
+    # Наведение на кнопку "О компании" и выполнение клика
     tensor_page.about_button.move_and_click()
+
+    # Проверка URL страницы, убеждаемся, что мы находимся на странице О компании
     assert tensor_page.get_current_url == "https://tensor.ru/about"
+
+    # Создание объекта страницы About для выполнения утверждений на ней
     about_page = About(browser)
 
+    # Проверка, что все четыре изображения на странице "О компании" имеют одинаковую ширину
     assert (
         about_page.image_1.get_attribute(value="width")
         == about_page.image_2.get_attribute(value="width")
         == about_page.image_3.get_attribute(value="width")
         == about_page.image_4.get_attribute(value="width")
     )
+
+    # Проверка, что все четыре изображения имеют одинаковую высоту
     assert (
         about_page.image_1.get_attribute(value="height")
         == about_page.image_2.get_attribute(value="height")
         == about_page.image_3.get_attribute(value="height")
         == about_page.image_4.get_attribute(value="height")
     )
-
--- tests/test_2.py
+++ tests/test_2.py
@@ -7,24 +7,40 @@ from src.pages.region_pop_up_page import RegionPopUp
 
 
 def test_2(browser: Remote) -> None:
+    # Создаём объект страницы Sbis
     sbis_page = Sbis(browser)
+    # Заходим на главную страницу Sbis.
     sbis_page.get("https://sbis.ru")
+    # Кликаем по кнопке для перехода на страницу контактов.
     sbis_page.contacts_button.click()
 
+    # Создаём объект страницы Cookie
     cookie_page = Cookie(browser)
+    # Закрываем всплывающее уведомление о куках на странице sbis.
     cookie_page.sbis_cookie_close_button.click()
 
+    # Переходим к работе со страницей контактов
     contact_page = Contacts(browser)
+    # Проверяем, что текст с регионом на странице контактов совпадает с "Ярославль".
     assert contact_page.region_text.text() == "Ярославль"
+    # Убеждаемся, что кнопка с регионом отображает текст "Ярославская обл.".
     assert contact_page.region_button.text() == "Ярославская обл."
+    # Проверяем, что текст на странице контактов корректно указывает на партнёра в Ярославле.
     assert contact_page.yaroslavl_partner_text.text() == "СБИС - Ярославль"
 
+    # Нажимаем на кнопку с регионом для вызова всплывающего окна с выбором региона.
     contact_page.region_button.click()
 
+    # Создаём объект всплывающего окна, чтобы выбрать другой регион.
     region_pop_up = RegionPopUp(browser)
+    # В окне выбора региона нажимаем на кнопку для перехода к Камчатскому краю.
     region_pop_up.kamchatka_button.click()
 
+    # Проверяем наличие элемента кнопки региона после изменения региона.
     contact_page.element_is_exist(contact_page.region_button.locator)
+    # Проверяем, что текст с текущим регионом теперь отображает "Петропавловск-Камчатский".
     assert contact_page.region_text.text() == "Петропавловск-Камчатский"
+    # Убедимся, что кнопка с регионом теперь показывает "Камчатский край". Это подтверждает успешную смену региона.
     assert contact_page.region_button.text() == "Камчатский край"
+    # Наконец, проверяем, что текст на странице корректно обновился, указывая на партнёра в Камчатском крае.
     assert contact_page.kamchatka_partner_text.text() == "СБИС - Камчатка"
